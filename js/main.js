41502cbaf222efe5b207db7391381af9fb5fdfce
Elefly13 <elymara13@gmail.com>
2021-07-22 17:40:31 +0200

    avanzamento lavoro

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
 // Creiamo un finto biglietto del treno con:
+var btnGenera = document.getElementById('calcolaPrezzo');
+console.log(btnGenera);
+
+btnGenera.addEventListener('click',
+    function() {
+        var nome = document.getElementById('nome').value;
+        var km = document.getElementById('km').value;
+        var fasciaEta = document.getElementById('fascia-eta').value;
+
+        var biglietto = document.getElementById('biglietto');
+        biglietto.style.display = 'block';
+    }
+)
+
+var btnAnnulla = document.getElementById('annulla');
+
+btnAnnulla.addEventListener('click',
+    function() {
+        var biglietto = document.getElementById('biglietto');
+        biglietto.style.display = "none";
+
+        document.getElementById('nome').value = '';
+        document.getElementById('km').value = '';
+        document.getElementById('fascia-eta').value = '';
+        }
+)
+
+
 // Nome passeggero
+
 // Codice treno (numero casuale tra 90000 e 100000 escluso)
 // Numero carrozza
 // Prezzo calcolato
